978334883e4fd235bde26f931e654078a65b36a4
weingrtn <janis.weingaertner@web.de>
2023-01-27 14:22:12 +0100

    Implements new function "ground_truth".

--- he_benchmarking/data/generator.py
+++ he_benchmarking/data/generator.py
@@ -6,13 +6,14 @@ class dataGenerator:
     def generate_int_data(size):
         x1 = np.random.randint(-1000000,1000000,size=size)
         x2 = np.random.randint(-1000000,1000000,size=size)
-        y = np.add(x1,x2)
 
-        return x1,x2,y
+        return x1,x2
 
     def generate_float_data(size):
         x1 = np.random.uniform(-100,100,size=size)
         x2 = np.random.uniform(-100,100,size=size)
-        y = np.add(x1,x2)
 
-        return x1,x2,y
+        return x1, x2
+
+    def ground_truth(x1, x2):
+        return np.add(x1, x2)
