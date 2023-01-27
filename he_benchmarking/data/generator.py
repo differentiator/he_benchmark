7efcad3b8e1f28a72f486cc6b32d80ce5795ad28
weingrtn <janis.weingaertner@web.de>
2023-01-27 15:15:55 +0100

    Adds init, class attr. and get/set functions.

--- he_benchmarking/data/generator.py
+++ he_benchmarking/data/generator.py
@@ -3,17 +3,37 @@ import numpy as np
 
 class dataGenerator():
 
+    def __init__(self, low, high):
+        self.low = low
+        self.high = high
+    
+    def getLow(self):
+        return self.low
+    
+    def setLow(self, low):
+        self.low = low
+
+    def getHigh(self):
+        return self.high
+    
+    def setHigh(self, high):
+        self.high = high
+
     def generate_int_data(self, size):
-        x1 = np.random.randint(-1000000,1000000,size=size)
-        x2 = np.random.randint(-1000000,1000000,size=size)
+        x1 = np.random.randint(self.low, self.high, size=size)
+        x2 = np.random.randint(self.low, self.high, size=size)
 
         return x1,x2
 
     def generate_float_data(self, size):
-        x1 = np.random.uniform(-100,100,size=size)
-        x2 = np.random.uniform(-100,100,size=size)
+        x1 = np.random.uniform(self.low, self.high, size=size)
+        x2 = np.random.uniform(self.low, self.high, size=size)
 
         return x1, x2
 
     def ground_truth(self, x1, x2):
         return np.add(x1, x2)
+
+
+
+
