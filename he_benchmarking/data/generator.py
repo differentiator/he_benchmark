ee01cb93727f45cc43e1f3a42fa78bc888b3844b
weingrtn <janis.weingaertner@web.de>
2023-01-27 15:00:33 +0100

    Fixes functions (adding "self" to parameters).

--- he_benchmarking/data/generator.py
+++ he_benchmarking/data/generator.py
@@ -1,19 +1,19 @@
 import numpy as np
 
 
-class dataGenerator:
+class dataGenerator():
 
-    def generate_int_data(size):
+    def generate_int_data(self, size):
         x1 = np.random.randint(-1000000,1000000,size=size)
         x2 = np.random.randint(-1000000,1000000,size=size)
 
         return x1,x2
 
-    def generate_float_data(size):
+    def generate_float_data(self, size):
         x1 = np.random.uniform(-100,100,size=size)
         x2 = np.random.uniform(-100,100,size=size)
 
         return x1, x2
 
-    def ground_truth(x1, x2):
+    def ground_truth(self, x1, x2):
         return np.add(x1, x2)
