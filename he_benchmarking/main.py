c439a467b7533445587acd13bbe079a2e1ce2a5d
differentiator <format.file96@gmail.com>
2023-02-04 19:27:09 +0100

    Pretty print results

--- he_benchmarking/main.py
+++ he_benchmarking/main.py
@@ -1,10 +1,12 @@
 import time
 import argparse
 import itertools
+import json
 from benchmarking.runner import Benchmark
 
 from utils.validator import str2bool
 import encryption
+
 
 # operations that can be compared at the moment are encryption and decryption, sum, multipliation and dot product.
 # Then, save and restoring (be carefull, this operation have to be executed and benchamarked toghether because the output of the first function differs)
@@ -13,6 +15,9 @@ import encryption
 # POSSIBLE SOLUTIONS --> 1. include in all the operations in Pyfhel that require a relinearization (i think just mult and dot) a relinearization so to have a fair comparison
 #                        2. find a way to relinearize manually in TENSeal
 #                        3. ignore the problem because I don't want to work on Data Security anymore :)
+
+def pretty_print(data):
+    print(json.dumps(data, indent=4))
 
 
 def main(args):
@@ -26,7 +31,7 @@ def main(args):
             print("ERROR: missing python module: " + module + "\n")
     benchmark = Benchmark(encryption_classes=classes, num_runs=args.number_of_runs)
     results = benchmark.run()
-    print(results)
+    pretty_print(results)
 
 
 if __name__ == "__main__":
